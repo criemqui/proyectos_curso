efe4105c5834fac6fd6a6f972737140ed8b9c3a7
criemqui <148879937+criemqui@users.noreply.github.com>
2023-10-27 12:11:59 -0500

    Update principal.py

--- principal.py
+++ principal.py
@@ -1,11 +1,27 @@
-num_entero = 20
-num_real = 3.014
-tpo cadena = "hola mundo"
-tipo_bool = True
+#tipo entero
+edad = 20
 
-print (num_entero)
-print (num_real)
-print (tipo_cadena)
-print (tipo_bool)
-resultado = 10+15
-print (resultado)
+#tipo cadena
+nombre = "cristina"
+
+#tipo decimal
+peso = 74.2
+
+#tipo boolean
+valor = True
+
+#guadar el resultado en una variable
+resultado = edad, nombre, peso, valor
+
+#mostrar el resultado
+print(resultado)
+
+
+#LIMITE DE ENTEROS
+#Pueden ser negativos o positivos o cero (0). Pueden realizarse operaciones entre ellos. 
+# Son el tipo base, por lo que se pueden realizar operaciones con otros tipos fácilmente. 
+# En Python 3 no hay límite de tamaño de estos números.
+
+#LIMITE DE FLOTANTES
+#Los float a diferencia de los int tienen unos valores mínimo y máximos que pueden representar. 
+#La mínima precisión es 2.2250738585072014e-308 y la máxima 1.7976931348623157e+308 , pero si no nos crees, lo puedes verificar tu mismo.
