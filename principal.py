3798e78c7985044fe096359b90496ee375082d40
criemqui <148879937+criemqui@users.noreply.github.com>
2023-10-26 19:30:57 -0500

    Update principal.py

--- principal.py
+++ principal.py
@@ -2,3 +2,10 @@ num_entero = 20
 num_real = 3.014
 tpo cadena = "hola mundo"
 tipo_bool = True
+
+print (num_entero)
+print (num_real)
+print (tipo_cadena)
+print (tipo_bool)
+resultado = 10+15
+print (resultado)
